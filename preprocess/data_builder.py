3fbc1a37f566b6192c4a6676b624ca67198e9d85
Anton Sukhin <treck199187@yandex.ru>
2022-02-11 02:14:25 +0300

    Add arg params for sentence and tokens len

--- preprocess/data_builder.py
+++ preprocess/data_builder.py
@@ -32,11 +32,11 @@ def create_json(
 
 
 class BertDatabuilder(object):
-    def __init__(self):
+    def __init__(self, args):
         self.tokenizer = BertTokenizer.from_pretrained(
             "sberbank-ai/ruBert-base", do_lower_case=True
         )
-
+        self.args = args
         self.sep_token = "[SEP]"
         self.cls_token = "[CLS]"
         self.pad_token = "[PAD]"
@@ -60,52 +60,61 @@ class BertDatabuilder(object):
 
         return segments_ids
 
-    def get_sent_labels(self, source, target):
+    def get_sent_labels(
+        self, source: List[List[str]], target: List[List[str]], idxs: List[int]
+    ) -> List[int]:
         sent_labels = greedy_selection(source, target, 3)
-        idxs = [i for i, _ in enumerate(source)]
 
         _sent_labels = [0] * len(source)
         for l in sent_labels:
             _sent_labels[l] = 1
         sent_labels = [_sent_labels[i] for i in idxs]
 
-        return sent_labels
+        return sent_labels[: self.args.max_src_nsents]
 
     def preprocess(self, data: List[Dict[str, str]]) -> SampleDict:
         datasets = []
         for d in data:
             src, tgt = d["src"], d["tgt"]
+            original_src_txt = [" ".join(s) for s in src]
+
+            idxs = [
+                i
+                for i, s in enumerate(src)
+                if (len(s) > self.args.min_src_ntokens_per_sent)
+            ]
+            sent_labels = self.get_sent_labels(
+                src, tgt, idxs
+            )
+
+            src = [src[i][: self.args.max_src_ntokens_per_sent] for i in idxs]
+            src = src[: self.args.max_src_nsents]
 
             src_txt = [" ".join(sent) for sent in src]
-            text = " ".join(
-                [
-                    self.cls_token + " " + " ".join(sent) + " " + self.sep_token
-                    for sent in src
-                ]
-            )
+            text = " {} {} ".format(self.sep_token, self.cls_token).join(src_txt)
 
             src_subtokens = self.tokenizer.tokenize(text)
 
+            src_subtokens = [self.cls_token] + src_subtokens + [self.sep_token]
             src_subtoken_idxs = self.tokenizer.convert_tokens_to_ids(src_subtokens)
-
             segments_ids = self.get_segments(src_subtoken_idxs)
-            cls_ids = [i for i, t in enumerate(src_subtoken_idxs) if t == self.cls_vid]
 
-            sent_labels = self.get_sent_labels(src, tgt)
+            cls_ids = [i for i, t in enumerate(src_subtoken_idxs) if t == self.cls_vid]
             sent_labels = sent_labels[: len(cls_ids)]
 
             tgt_subtokens_str = (
                 "[unused3] "
                 + " [unused2] ".join(
-                    [" ".join(self.tokenizer.tokenize(" ".join(tt),)) for tt in tgt]
+                    [" ".join(self.tokenizer.tokenize(" ".join(tt))) for tt in tgt]
                 )
                 + " [unused1]"
             )
-            tgt_subtoken = tgt_subtokens_str.split()
+            tgt_subtoken = tgt_subtokens_str.split()[: self.args.max_tgt_ntokens]
 
             tgt_subtoken_idxs = self.tokenizer.convert_tokens_to_ids(tgt_subtoken)
 
             tgt_txt = "<q>".join([" ".join(tt) for tt in tgt])
+            src_txt = [original_src_txt[i] for i in idxs]
 
             b_data_dict = {
                 "src": src_subtoken_idxs,
