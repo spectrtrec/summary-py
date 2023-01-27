0cb5142b5069f4aaa2f08ff958c56af2dde1c839
user <user>
2023-01-27 15:09:10 +0000

    Fix typing in modules

--- models/data_loader.py
+++ models/data_loader.py
@@ -1,73 +1,86 @@
 import bisect
-
+from omegaconf import DictConfig
 import pytorch_lightning as pl
 import torch
-from typing import List
+from typing import List, Union, MutableMapping, Optional, Any
 from collections import defaultdict
 
-from torch.utils.data import DataLoader, RandomSampler
+from torch.utils.data import DataLoader, RandomSampler  # type: ignore
 from utils.typing_hint import SampleDict
+from pytorch_lightning.utilities.parsing import AttributeDict
+from torch.utils.data.dataset import Dataset
 
 
 class DataModule(pl.LightningDataModule):
-    def __init__(self, args, train_dict=None, val_dict=None, test_dict=None):
+    def __init__(
+        self,
+        args: Union[DictConfig, Union[AttributeDict, MutableMapping]],
+        train_dict: Optional[Dataset]=None,
+        val_dict: Optional[Dataset]=None,
+        test_dict: Optional[Dataset]=None,
+    ) -> None:
         super(DataModule, self).__init__()
         self.save_hyperparameters(args)
-        self._train_dataset = train_dict
-        self._dev_dataset = val_dict
-        self._test_dataset = test_dict
+        self._train_dataset: Optional[Dataset] = train_dict
+        self._dev_dataset: Optional[Dataset] = val_dict
+        self._test_dataset: Optional[Dataset] = test_dict
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
-            dataset=self._train_dataset,
-            sampler=RandomSampler(self._train_dataset),
-            batch_size=self.hparams.batch_size,
+            dataset=self._train_dataset,  # type: ignore
+            sampler=RandomSampler(self._train_dataset),  # type: ignore
+            batch_size=self.hparams.batch_size,  # type: ignore
             collate_fn=self.prepare_sample,
-            num_workers=self.hparams.loader_workers,
+            num_workers=self.hparams.loader_workers,  # type: ignore
             drop_last=True,
         )
 
     def val_dataloader(self) -> DataLoader:
         return DataLoader(
-            dataset=self._dev_dataset,
-            batch_size=self.hparams.batch_size,
+            dataset=self._dev_dataset,  # type: ignore
+            batch_size=self.hparams.batch_size,  # type: ignore
             collate_fn=self.prepare_sample,
-            num_workers=self.hparams.loader_workers,
+            num_workers=self.hparams.loader_workers,  # type: ignore
             drop_last=True,
         )
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(
-            dataset=self._test_dataset,
-            batch_size=self.hparams.batch_size,
+            dataset=self._test_dataset,  # type: ignore
+            batch_size=self.hparams.batch_size,  # type: ignore
             collate_fn=self.prepare_sample,
-            num_workers=self.hparams.loader_workers,
+            num_workers=self.hparams.loader_workers,  # type: ignore
             drop_last=True,
         )
 
     def prepare_sample(
-        self, sample: List[SampleDict], is_test: bool = False,
+        self,
+        sample: List[SampleDict],
+        is_test: bool = False,
     ) -> SampleDict:
-        sample = self.collate_data(sample)
-        src = sample["src"]
-        tgt = [tgts[: self.hparams.max_tgt_len][:-1] + [2] for tgts in sample["tgt"]]
-        src_sent_labels = sample["src_sent_labels"]
-        segs = sample["segs"]
-        if not self.hparams.use_interval:
-            segs = [[0] * len(seg) for seg in segs]
-        clss = sample["clss"]
-        src_txt = sample["src_txt"]
-        tgt_txt = sample["tgt_txt"]
-
-        end_id = [source[-1] for source in src]
-        src = [
-            source[:-1][: self.hparams.max_pos - 1] + [end_id[i]]
-            for i, source in enumerate(src)
+        collate_sample: SampleDict = self.collate_data(sample)
+        src:List[List[int]] = collate_sample["src"]
+        tgt:List[List[int]] = [
+            tgts[: self.hparams.max_tgt_len][:-1] + [2] # type: ignore
+            for tgts in collate_sample["tgt"] 
         ]
-        segs = [source[: self.hparams.max_pos] for source in segs]
+        src_sent_labels:List[List[int]] = collate_sample["src_sent_labels"]
+        segs:List[List[int]] = collate_sample["segs"] 
+        if not self.hparams.use_interval: # type: ignore
+            segs = [[0] * len(seg) for seg in segs]
+        clss:List[List[int]] = collate_sample["clss"] 
+        src_txt:List[List[str]] = collate_sample["src_txt"]
+        tgt_txt:List[List[str]] = collate_sample["tgt_txt"]
 
-        max_sent_id = [
-            bisect.bisect_left(source, self.hparams.max_pos) for source in clss
+        end_id: list[int] = [source[-1] for source in src]
+        src = [
+            source[:-1][: self.hparams.max_pos - 1] + [end_id[i]] # type: ignore
+            for i, source in enumerate(src)
+        ] 
+        segs = [source[: self.hparams.max_pos] for source in segs] # type: ignore
+
+        max_sent_id: list[int] = [
+            bisect.bisect_left(source, self.hparams.max_pos) for source in clss # type: ignore
         ]
 
         src_sent_labels = [
@@ -75,43 +88,46 @@ class DataModule(pl.LightningDataModule):
         ]
         clss = [source[: max_sent_id[i]] for i, source in enumerate(clss)]
 
-        src = torch.tensor(self._pad(src, 0))
-        tgt = torch.tensor(self._pad(tgt, 0))
+        src = self._pad(src, 0) # type: ignore
+        tgt = self._pad(tgt, 0) # type: ignore
 
-        segs = torch.tensor(self._pad(segs, 0))
+        segs = self._pad(segs, 0) # type: ignore
 
-        mask_src = torch.stack([~(source == 0) for source in src])
-        mask_tgt = torch.stack([~(source == 0) for source in tgt])
+        mask_src = torch.stack([~(source == 0) for source in src]) # type: ignore
+        mask_tgt = torch.stack([~(source == 0) for source in tgt]) # type: ignore
 
-        clss = torch.tensor(self._pad(clss, -1))
-        src_sent_labels = torch.tensor(self._pad(src_sent_labels, 0))
+        clss = self._pad(clss, -1) # type: ignore
+        src_sent_labels = self._pad(src_sent_labels, 0) # type: ignore
 
-        mask_cls = torch.stack([~(source == -1) for source in clss])
+        mask_cls = torch.stack([~(source == -1) for source in clss]) # type: ignore
         clss = torch.tensor(
             [[0 if num == -1 else num for num in source] for source in clss]
+        ) # type: ignore
+        return SampleDict(
+            {
+                "src": src,
+                "tgt": tgt,
+                "src_sent_labels": src_sent_labels,
+                "segs": segs,
+                "clss": clss,
+                "mask_src": mask_src,
+                "mask_cls": mask_cls,
+                "mask_tgt": mask_tgt,
+                "src_txt": src_txt,
+                "tgt_txt": tgt_txt,
+            }
         )
-        return {
-            "src": src,
-            "tgt": tgt,
-            "src_sent_labels": src_sent_labels,
-            "segs": segs,
-            "clss": clss,
-            "mask_src": mask_src,
-            "mask_cls": mask_cls,
-            "mask_tgt": mask_tgt,
-            "src_txt": src_txt,
-            "tgt_txt": tgt_txt,
-        }
 
-    def _pad(self, data: List[List[int]], pad_id: int, width=-1) -> List[List[int]]:
-        if width == -1:
-            width = max(len(d) for d in data)
-        rtn_data = [d + [pad_id] * (width - len(d)) for d in data]
-        return rtn_data
+    def _pad(self, data: List[List[int]], pad_id: int, width=-1) -> torch.Tensor:
+        return torch.tensor((
+            [d + [pad_id] * (max(len(d) for d in data) - len(d)) for d in data]
+            if width == -1
+            else [d + [pad_id] * (width - len(d)) for d in data]
+        ))
 
     @staticmethod
-    def collate_data(data_batch: List[SampleDict]) -> List[SampleDict]:
-        collate_dict = defaultdict(list)
+    def collate_data(data_batch: List[SampleDict]) -> SampleDict:
+        collate_dict: defaultdict[str, List[Any]] = defaultdict(list)
         for data in data_batch:
             collate_dict["src"].append(data["src"])
             collate_dict["tgt"].append(data["tgt"])
@@ -120,4 +136,4 @@ class DataModule(pl.LightningDataModule):
             collate_dict["clss"].append(data["clss"])
             collate_dict["src_txt"].append(data["src_txt"])
             collate_dict["tgt_txt"].append(data["tgt_txt"])
-        return collate_dict
+        return SampleDict(collate_dict)
